460eb8d944a0aae07121742495fcfe311bedb108
utkarsh <singhrishu680@gmail.com>
2023-06-19 14:14:35 +0530

    done with make ringtone snooze and some changes in delete function

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ var am_pm_text_display = document.getElementById("am-pm-text");
 var dateDisplay = document.querySelector(".date");
 
 const audio = new Audio('/music.mp3');
+var stopText = document.querySelector(".stop-text");
+var stopSnooze = document.getElementById('stop-alarm');
 
 // for changin am pm
 var am_pm_checkbox = document.getElementById('am-pm-checkbox');
@@ -46,12 +48,13 @@ var alarmTime = {
   }
 
 
-function updateTime(){
+function updateTime(alarmTime){
 let date = new Date();
 let hours = formateTime(date.getHours());
 let minutes = formateTime(date.getMinutes());
 let seconds = formateTime(date.getSeconds());
 dateDisplay.innerHTML =`${date}`
+clockDisplay.innerHTML = `${hours}:${minutes}:${seconds}`;
 
 //for am and pm
 if(hours >= 12){
@@ -59,14 +62,12 @@ if(hours >= 12){
 }else{
     am_pm_text_display.innerHTML = "AM";
 }
-
-
 var twelve = document.getElementById('twelve');
 if(twelveHour==true){
     twelve.innerText = '12';
     if(hours > 12){
     hours = hours-12;
-    clockDisplay.innerHTML = `${hours}:${minutes}:${seconds}`;
+    clockDisplay.innerHTML = `0${hours}:${minutes}:${seconds}`;
     }
     else if(hours == 0){
     hours = 12;
@@ -83,17 +84,16 @@ if(twelveHour==true){
 for(let i = 0; i < AlarmArray.length; i++){
     if(hours == AlarmArray[i].hour && minutes == AlarmArray[i].minute && seconds == AlarmArray[i].second && amPm == AlarmArray[i].amPm){
         console.log(alarmTime)
-        window.alert("alarm is ringing")
         audio.play();
-        
+        stopText.style.display = 'block';
+        stopSnooze.style.display = 'block';
+        successNoty("Alarm is ringing");
         setTimeout(function(){
-            audio.pause();
-            console.log("alarm is stoped")
+            stopAlarm(); 
         }
-        , 5000);
+        , 1000000);     
     }
 }
-
 }
 //here we use setInterval to update the clock every second
 setInterval(updateTime, 1000);
@@ -135,13 +135,23 @@ function setAlarm(){
         second.value = '';
         return;
     }
-    if(hour.value <9 || minute.value < 9 || second.value < 9){
-        warningNoty('Please add 0 before single digit');
+    if(hour.value < 0 || minute.value < 0 || second.value < 0){
+        warningNoty('@hr(0-24), min(0-59), sec (0, 59)');
         hour.value = '';
         minute.value = '';
         second.value = '';
         return;
     }
+    if(hour.value < 10 && hour.value.length < 2){
+        hour.value = '0' + hour.value;
+    }
+    if(minute.value < 10 && minute.value.length < 2){
+        minute.value = '0' + minute.value;
+    }
+    if(second.value < 10 && second.value.length < 2){
+        second.value = '0' + second.value;
+    }
+   
     alarmTime = {
         hour: hour.value,
         minute: minute.value,
@@ -151,25 +161,29 @@ function setAlarm(){
     }
     console.log(alarmTime)
     AlarmArray.push(alarmTime);
-    if(AlarmArray.push(alarmTime)){
-        successNoty("Alarm is set");
-    }
-    addAlarmList(alarmTime);
+    successNoty("Alarm is set");
+    renderAlarmList();  
     hour.value = '';
     minute.value = '';
     second.value = '';
-    am_pm_checkbox.classList.remove('active');
-    updateTime(alarmTime);
-    
+    am_pm_checkbox.classList.remove('active');    
+}
+
+// for stop alarm
+function stopAlarm(){
+    audio.pause();
+    stopText.style.display = 'none';
+    stopSnooze.style.display = 'none';   
+    successNoty("Alarm is stopped");
+    audio.currentTime = 0;
 }
 
 
 var ul = document.querySelector('#alarm-list');
 function addAlarmList(alarmTime){
-
-    const Li = document.createElement('div');
-    Li.classList.add('list-box');
-    Li.innerHTML = `
+    const li = document.createElement('list');
+    li.classList.add('list-box');
+    li.innerHTML = `
     <span class = 'alarm-hour'>${alarmTime.hour}</span>
     <span class = 'li-column'>:</span>
     <span class = 'alarm-minute'>${alarmTime.minute}</span>
@@ -177,22 +191,10 @@ function addAlarmList(alarmTime){
     <span class = 'alarm-second'>${alarmTime.second}</span>
     <span class = 'alarm-am-pm'>${alarmTime.amPm}</span>
     <span class ='alarm-delete' ${alarmTime.id}>
-        <i class="fa-solid fa-trash" id = "delete" data-id="${alarmTime.id}"></i>
+        <i class="fa-solid fa-trash" id = "delete" data-id="${alarmTime.id}")></i>
     </span>`;
-    ul.appendChild(Li);
+    ul.append(li);
   }
-
-  // render alarm list
-  function renderAlarmList(){
-    console.log("render")
-    // localStorage.setItem('LocalStorage', JSON.stringify(AlarmArray));
-    ul.innerHTML = '';
-    for(let element of AlarmArray){
-      addAlarmList(element);
-    }
-  }
-
-
 //  clear all alarm
   function clearAlarm(){
     ul.innerHTML = '';
@@ -200,27 +202,33 @@ function addAlarmList(alarmTime){
   }
 
 // delete alarm
-    function deleteItem(id){
-        let newAlarmArray = AlarmArray.filter(function(alarm){
-        if(alarm.id != id){
-            return alarm;
-        }
-    });
-    AlarmArray = newAlarmArray;
-    renderAlarmList();
-    }
-
-
-  function deleteAlarm(e){
-   if(e.target.id == 'delete'){
-    let id = e.target.dataset.id;
-    console.log(id)
-    deleteItem(id)
-   }
+function deleteItem(deleteId){
+  console.log('delete')
+  let newAlarmArray = AlarmArray.filter(function(alarm){
+    return alarm.id !== Number(deleteId);
+  });
+  AlarmArray = newAlarmArray;
+  renderAlarmList();
+  successNoty("Alarm is deleted");
+    
   }
+  
+document.addEventListener('click', function(e){
+    if(e.target.id == 'delete'){
+      let deleteId = e.target.dataset.id;
+      deleteItem(deleteId);
+    }
+  });
 
-document.addEventListener('click', deleteAlarm);
-
+    // render alarm list
+    function renderAlarmList(){
+      console.log("render")
+      localStorage.setItem('LocalStorage', JSON.stringify(AlarmArray));
+      ul.innerHTML = '';
+      for(let element of AlarmArray){
+        addAlarmList(element);
+      }
+    }
 
 // izitoast
 function successNoty(msg) {
