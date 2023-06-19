2f7642fc9455fc523a36178e1800b574abb8ac9a
utkarsh <singhrishu680@gmail.com>
2023-06-19 15:25:16 +0530

    done

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 var clockDisplay = document.getElementById('clock-display');
 var am_pm_text_display = document.getElementById("am-pm-text");
 var dateDisplay = document.querySelector(".date");
-
 const audio = new Audio('/alarmTone/alarm_music.mp3');
 var stopText = document.querySelector(".stop-text");
 var stopSnooze = document.getElementById('stop-alarm');
@@ -21,33 +20,23 @@ am_pm_checkbox.addEventListener('click', function(){
         console.log(amPm);
     }
 });
+
 var hour = document.getElementById('hour');
 var minute = document.getElementById('minute');
 var second = document.getElementById('second');
 
-
 audio.loop = true;
 let AlarmArray = [];
 
-// let retrieveAlarm = JSON.parse(localStorage.getItem('LocalStorage'));
-
-// if (retrieveAlarm !== null) {
-//     AlarmArray = [...retrieveAlarm];
-//     renderAlarmList();
-// }
-
-
-//  local storage
-
 var alarmTime = {
-    hour: null,
-    minute: null,
-    second: null,  
-    id: Date.now(),
-    amPm,
-  }
+  hour: null,
+  minute: null,
+  second: null,  
+  id: Date.now(),
+  amPm,
+}
 
-
+// function upadate time
 function updateTime(alarmTime){
 let date = new Date();
 let hours = formateTime(date.getHours());
@@ -81,7 +70,7 @@ if(twelveHour==true){
 }
 
 // for alarm array
-for(let i = 0; i < AlarmArray.length; i++){
+  for(let i = 0; i < AlarmArray.length; i++){
     if(hours == AlarmArray[i].hour && minutes == AlarmArray[i].minute && seconds == AlarmArray[i].second && amPm == AlarmArray[i].amPm){
         console.log(alarmTime)
         audio.play();
@@ -93,80 +82,76 @@ for(let i = 0; i < AlarmArray.length; i++){
         }
         , 1000000);     
     }
+  }
 }
-}
+
 //here we use setInterval to update the clock every second
 setInterval(updateTime, 1000);
 
 //function for applying 0 before single digit
 function formateTime(time) {
-    if (time < 10) {
-      return `0${time}`;
-    } else {
-      return time;
-    }
+  if (time < 10) {
+    return `0${time}`;
+  } else {
+    return time;
+  }
 }
 
 // for time formate of 12 hours and 24 hours
- 
 let twelveHour = true;
-
 let mode = document.getElementById("mode");
-
 mode.addEventListener("click", function(){
     twelveHour = !twelveHour;
 });
 
-
 // for set alarm function
-
 function setAlarm(){
-    if(hour.value == '' || minute.value == '' || second.value == ''){
-        warningNoty("Please fill all fields");
-        hour.value = '';
-        minute.value = '';
-        second.value = '';
-        return;
-    }
-    if(hour.value > 24 || minute.value > 59 || second.value > 59){
-        warningNoty('@hr(0-24), min(0-59), sec (0, 59)');
-        hour.value = '';
-        minute.value = '';
-        second.value = '';
-        return;
-    }
-    if(hour.value < 0 || minute.value < 0 || second.value < 0){
-        warningNoty('@hr(0-24), min(0-59), sec (0, 59)');
-        hour.value = '';
-        minute.value = '';
-        second.value = '';
-        return;
-    }
-    if(hour.value < 10 && hour.value.length < 2){
-        hour.value = '0' + hour.value;
-    }
-    if(minute.value < 10 && minute.value.length < 2){
-        minute.value = '0' + minute.value;
-    }
-    if(second.value < 10 && second.value.length < 2){
-        second.value = '0' + second.value;
-    }
-   
-    alarmTime = {
-        hour: hour.value,
-        minute: minute.value,
-        second: second.value,
-        amPm,
-        id: Date.now()
-    }
-    console.log(alarmTime)
-    AlarmArray.push(alarmTime);
-    successNoty("Alarm is set");
-    renderAlarmList();  
-    hour.value = '';
-    minute.value = '';
-    second.value = '';
-    am_pm_checkbox.classList.remove('active');    
+  if(hour.value == '' || minute.value == '' || second.value == ''){
+      warningNoty("Please fill all fields");
+      hour.value = '';
+      minute.value = '';
+      second.value = '';
+      return;
+  }
+  if(hour.value > 24 || minute.value > 59 || second.value > 59){
+      warningNoty('@hr(0-24), min(0-59), sec (0, 59)');
+      hour.value = '';
+      minute.value = '';
+      second.value = '';
+      return;
+  }
+  if(hour.value < 0 || minute.value < 0 || second.value < 0){
+      warningNoty('@hr(0-24), min(0-59), sec (0, 59)');
+      hour.value = '';
+      minute.value = '';
+      second.value = '';
+      return;
+  }
+  if(hour.value < 10 && hour.value.length < 2){
+      hour.value = '0' + hour.value;
+  }
+  if(minute.value < 10 && minute.value.length < 2){
+      minute.value = '0' + minute.value;
+  }
+  if(second.value < 10 && second.value.length < 2){
+      second.value = '0' + second.value;
+  }
+  
+  alarmTime = {
+      hour: hour.value,
+      minute: minute.value,
+      second: second.value,
+      amPm,
+      id: Date.now()
+  }
+  console.log(alarmTime)
+  AlarmArray.push(alarmTime);
+  successNoty("Alarm is set");
+  renderAlarmList();  
+  hour.value = '';
+  minute.value = '';
+  second.value = '';
+  am_pm_checkbox.classList.remove('active');    
 }
 
 // for stop alarm
@@ -178,7 +163,7 @@ function stopAlarm(){
   successNoty("Alarm is stopped");
 }
 
-
+// for show alarms
 var ul = document.querySelector('#alarm-list');
 function addAlarmList(alarmTime){
     const li = document.createElement('list');
@@ -195,11 +180,12 @@ function addAlarmList(alarmTime){
     </span>`;
     ul.append(li);
   }
+
 //  clear all alarm
-  function clearAlarm(){
-    ul.innerHTML = '';
-    AlarmArray = [];
-  }
+function clearAlarm(){
+  ul.innerHTML = '';
+  AlarmArray = [];
+}
 
 // delete alarm
 function deleteItem(deleteId){
@@ -210,39 +196,49 @@ function deleteItem(deleteId){
   AlarmArray = newAlarmArray;
   renderAlarmList();
   successNoty("Alarm is deleted");
-    
-  }
+}
   
 document.addEventListener('click', function(e){
-    if(e.target.id == 'delete'){
-      let deleteId = e.target.dataset.id;
-      deleteItem(deleteId);
-    }
-  });
+  if(e.target.id == 'delete'){
+    let deleteId = e.target.dataset.id;
+    deleteItem(deleteId);
+  }
+});
 
-    // render alarm list
-    function renderAlarmList(){
-      console.log("render")
-      localStorage.setItem('LocalStorage', JSON.stringify(AlarmArray));
-      ul.innerHTML = '';
-      for(let element of AlarmArray){
-        addAlarmList(element);
-      }
-    }
+// render alarm list
+function renderAlarmList(){
+  console.log("render")
+  localStorage.setItem('LocalStorage', JSON.stringify(AlarmArray));
+  ul.innerHTML = '';
+  for(let element of AlarmArray){
+    addAlarmList(element);
+  }
+}
+
+// console the developer info
+console.log(
+  "%c Hey! it's Utkarsh Singh,",
+  "color : #00000; font-size : 1rem; font-weight : bold;"
+);
+console.log(
+  "%c Passionate Full Stack Developer.",
+  "color : #00000; font-size : 0.7rem; font-weight : bold;"
+);
+
 
 // izitoast
 function successNoty(msg) {
-    iziToast.success({
-      title: msg,
-      position: "topRight",
-      timeout: 2000,
-    });
-  }
+  iziToast.success({
+    title: msg,
+    position: "topRight",
+    timeout: 2000,
+  });
+}
   
-  function warningNoty(msg) {
-    iziToast.warning({
-      title: msg,
-      position: "topRight",
-      timeout: 2000,
-    });
-  }
+function warningNoty(msg) {
+  iziToast.warning({
+    title: msg,
+    position: "topRight",
+    timeout: 2000,
+  });
+}
